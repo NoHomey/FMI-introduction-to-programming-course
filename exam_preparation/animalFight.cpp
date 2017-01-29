eb955ecbf9f20f97b4d3a5759880042bfa60d0b8
Ivo Stratev <ivo.stratev.tues@gmail.com>
2017-01-29 13:22:38 +0200

    Update animalFight.cpp

--- exam_preparation/animalFight.cpp
+++ exam_preparation/animalFight.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <iostream>
+#include <new>
 
 #define MAX(a, b) (a < b ? b : a)
 
@@ -7,7 +8,7 @@ const float EPSILON = 0.001;
 
 class Animal {
 protected:
-float id;
+size_t id;
 float power;
 float agility;
 float haste;
@@ -19,7 +20,7 @@ static unsigned short random() {
 }
 
 float hitChance(const Animal* oponent) const {
-    return (1.0 + (knowledge / 100.0)) * (MAX(10, (agility - oponent->agility)) / 100.0) * 100.00;
+    return (1.0 + (knowledge / 100.0)) * (MAX(10, (agility - oponent->agility)) / 100.0) * 10.00;
 }
 
 float hitDamage(const Animal* oponent) const {
@@ -27,15 +28,11 @@ float hitDamage(const Animal* oponent) const {
 }
 
 public:
-Animal(const float Id, const float Power, const float Agility, const float Haste, const float Knowledge, const float Health)
+Animal(const size_t Id, const float Power, const float Agility, const float Haste, const float Knowledge, const float Health)
 : id{Id}, power{Power}, agility{Agility}, haste{Haste}, knowledge{Knowledge}, health{Health} {}
 
 float attack(const Animal* oponent) const {
-    const float chance = hitChance(oponent);
-    const int hit = random();
-    const float damage = hitDamage(oponent);
-    std::cout << chance << ' ' << hit << ' ' << damage << std::endl;
-    return (chance - hit) > EPSILON ? damage : 0.0;
+    return (hitChance(oponent) - random()) > EPSILON ? hitDamage(oponent) : 0.0;
 }
 
 void takeDamage(const float Damage) {
@@ -45,22 +42,109 @@ void takeDamage(const float Damage) {
 bool isAlive() const {
     return health > EPSILON;
 }
+
+size_t getId() const {
+    return id;
+}
 };
 
-int main() {
-    srand(time(NULL));
-    Animal animal1 = {1, 70, 80, 35, 70, 100};
-    Animal animal2 = {2, 45, 50, 80, 10, 100};
-    Animal* attacking = &animal1;
-    Animal* attacked = &animal2;
+class Arena {
+protected:
+static void alloc(Animal**& animals, size_t count) {
+    try {
+        animals = new Animal*[count];
+    } catch(std::bad_alloc& error) {
+        throw error;
+    }
+}
+
+static Animal* figth(Animal* animal1, Animal* animal2) {
+    Animal* attacking = animal1;
+    Animal* attacked = animal2;
     Animal* tmp = nullptr;
-    while(animal1.isAlive() and animal2.isAlive()) {
+    while(animal1->isAlive() and animal2->isAlive()) {
         attacked->takeDamage(attacking->attack(attacked));
         tmp = attacking;
         attacking = attacked;
         attacked = tmp;
     }
-    std::cout << animal1.isAlive() << ' ' << animal2.isAlive() << std::endl;
+    delete attacking;
+
+    return attacked;
+}
+
+size_t currentFigtherId;
+size_t figthersCount;
+Animal** figthers;
+Animal** winners;
+
+void allocWinners() {
+    alloc(winners, figthersCount >> 1);
+}
+
+public:
+static void initialize() {
+    srand(time(NULL));
+}
+
+Arena(const size_t FigthersCount)
+: currentFigtherId{0}, figthersCount{FigthersCount}, figthers{nullptr}, winners{nullptr} {
+    alloc(figthers, FigthersCount);
+    allocWinners();
+}
+
+~Arena() {
+    delete[] figthers;
+    delete[] winners;
+}
+
+void addFighter(const float Power, const float Agility, const float Haste, const float Knowledge, const float Health) {
+    figthers[currentFigtherId++] = new Animal{currentFigtherId, Power, Agility, Haste, Knowledge, Health};
+}
+
+bool preparaForNextRound() {
+    if(figthersCount == 2) {
+        return false;
+    }
+    delete[] figthers;
+    figthers = winners;
+    figthersCount >>= 1;
+    allocWinners();
+
+    return true;
+}
+
+void round() {
+    for(size_t i = 0; i < figthersCount; i += 2) {
+        winners[i / 2] = figth(figthers[i], figthers[i + 1]);
+    }
+}
+
+void start() {
+    while(true) {
+        round();
+        if(!preparaForNextRound()) {
+            return;
+        }
+    }
+}
+
+Animal* getWinner() const {
+    return winners[0];
+}
+};
+
+int main() {
+    Arena::initialize();
+    Arena forest = {4};
+    forest.addFighter(20, 59, 23.1, 37.4, 200);
+    forest.addFighter(20, 59, 23.1, 37.4, 200);
+    forest.addFighter(20, 59, 23.1, 37.4, 200);
+    forest.addFighter(20, 59, 23.1, 37.4, 200);
+    forest.start();
+    Animal* winner = forest.getWinner();
+    std::cout << winner->getId() << std::endl;
+    delete winner;
 
     return 0;
 }
